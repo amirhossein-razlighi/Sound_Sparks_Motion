5e8d06b9f045ae6dbf7b483868ae3de143d6f121
amirhossein-razlighi <arazlighi@gmail.com>
2026-09-07 00:28:24 -0400

    ab_sheet: single-threaded encoder, even heights

--- h3_probe/scenarios/ab_sheet.sh
+++ h3_probe/scenarios/ab_sheet.sh
@@ -8,7 +8,7 @@ for v in "$@"; do
   nf=$(ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_read_frames -of csv=p=0 "$v")
   s=${START:-0}; e=${END:-$((nf-1))}; step=$(( (e - s) / (N - 1) )); [ $step -lt 1 ] && step=1
   cropf=""; [ -n "${CROP:-}" ] && cropf="crop=$CROP,"
-  args+=(-i "$v"); filt="$filt[$i:v]select='between(n\,$s\,$e)*not(mod(n-$s\,$step))',${cropf}scale=$W:-1,tile=${N}x1[r$i];"; i=$((i+1))
+  args+=(-i "$v"); filt="$filt[$i:v]select='between(n\,$s\,$e)*not(mod(n-$s\,$step))',${cropf}scale=$W:-2,tile=${N}x1[r$i];"; i=$((i+1))
 done
 rows=""; for ((k=0;k<i;k++)); do rows="$rows[r$k]"; done
-ffmpeg -y -v error -threads 1 "${args[@]}" -filter_complex "$filt${rows}vstack=inputs=$i" -frames:v 1 -q:v 4 "$out"   # .png or .jpg (jpg is ~10x smaller)
+ffmpeg -y -v error -threads 1 "${args[@]}" -filter_complex "$filt${rows}vstack=inputs=$i" -frames:v 1 -q:v 4 -threads 1 "$out"   # .png or .jpg (jpg is ~10x smaller)
